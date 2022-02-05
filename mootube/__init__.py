94e5a4ef271bf14e43f8ff27cbb591f016d3933b
Jake Day <jake@ninebysix.com>
2022-02-05 15:06:51 -0500

    fix player import

--- mootube/__init__.py
+++ mootube/__init__.py
@@ -14,7 +14,7 @@ from PIL import Image
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GdkPixbuf, Gio, GLib
 
-from player import MediaPlayer
+from .player import MediaPlayer
 
 class MooTube(Gtk.Window):
     def __init__(self):
